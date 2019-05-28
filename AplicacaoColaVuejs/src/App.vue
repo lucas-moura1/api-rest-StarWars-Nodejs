9043e09285f93a249c2d0be8a1f855cadf55d6f7
lucas3ms <lucas3ms.ls@gmail.com>
2019-05-28 08:49:51 -0300

    Começando a aplicação em Vuejs

---
+++ AplicacaoColaVuejs/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por Título">
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.id">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulofoto">
+        </meu-painel>
+            
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from './components/shared/painel/Painel.vue';
+
+export default {
+
+  components: {
+    'meu-painel' : Painel
+  },
+
+  data() {
+    return {
+      titulo: "ProjetoVue",
+      fotos: [],
+      filtro: ''
+    };
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
+  }
+};
+</script>
+
+<style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva{
+    width: 100%;
+  }
+
+  .filtro{
+    display: block;
+    width: 100%;
+    padding: 0.5em;
+    align-items: center;
+    margin: 2em;
+    font: 1em normal; 
+  }
+</style>
