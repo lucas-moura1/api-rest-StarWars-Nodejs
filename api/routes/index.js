c002f2ab5dd65c66f287d1b10e6e3c471c8cf1d0
Lucas <lucas3ms.ls@gmail.com>
2019-05-28 00:54:23 -0300

    Configuração no metodo GET e ajustes no Banco

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api', function(req, res, next) {
+  var db = require('../db');
+  var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+  Customer.find({}).lean().exec(function(e,docs){
+    res.json(docs);
+    res.end();
+  });  
 });
 
 module.exports = router;
