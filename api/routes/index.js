1c5302f55b40c276485499307d2e7d2f82326310
lucas3ms <lucas3ms.ls@gmail.com>
2019-05-29 13:55:08 -0300

    inserindo busca com nome

--- api/routes/index.js
+++ api/routes/index.js
@@ -14,19 +14,19 @@ router.get('/api', function(req, res, next) {
 router.get('/api/:nome', function(req, res, next) {
   var db = require('../db');
   var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
-  Customer.find({nome: req.params.nome}).lean().exec(function(e,docs){
+  Customer.find({ nome: req.params.nome}).lean().exec(function(e,docs){
     res.json(docs);
     res.end();
   });  
 });
 
-router.get('/api/:id', function(req, res, next) {
+router.get('/api/:id', function (req, res, next) {
   var db = require('../db');
   var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
-  Customer.find({_id: req.params.nome}).lean().exec(function(e,docs){
-    res.json(docs);
-    res.end();
-  });  
+  Customer.find({ _id: req.params.id }).lean().exec(function (e, docs) {
+      res.json(docs);
+      res.end();
+  });
 });
 
 router.post('/api/', function (req, res, next) {
