0835ade4ffea389c15c21276277d531b823bbbd2
lucas3ms <lucas3ms.ls@gmail.com>
2019-06-24 17:19:23 -0300

    atualizando o schema

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ var customerSchema = new mongoose.Schema({
     nome: String,
     clima: String,
     terreno: String
-}, { collection: 'customers' }
+}, { collection: 'customers' },
+    {versionKey: false}
 );
 
 module.exports = { Mongoose: mongoose, CustomerSchema: customerSchema }
