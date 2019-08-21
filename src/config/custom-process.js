beacb91d716dfe7074adae0bbe574b3e9f9628e3
lucas3ms <lucas3ms.ls@gmail.com>
2019-08-21 17:09:29 -0300

    Organization the files

--- src/config/custom-process.js
+++ src/config/custom-process.js
@@ -1,10 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('../app/routes/routes');
 
 var app = express();
 
@@ -19,11 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
