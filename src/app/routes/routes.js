8b9504653eb4a4a95424613003aeb3a9f1b8af9e
lucas3ms <lucas3ms.ls@gmail.com>
2020-01-23 10:27:17 -0300

    Refatoração das rotas

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,61 +1,70 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const planet_model = require('../../config/db')
 
-/* GET home page. */
-router.get('/api', function(req, res, next) {
-  const db = require('../db');
-  const Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
-  Customer.find({}).lean().exec(function(e,docs){
-    res.json(docs);
-    res.end();
-  });  
-});
+router.get('/planet', (req, res) => {
+  planet_model.find({}).lean().exec()
+    .then(data => {   
+      res.json(data)
 
-router.get('/api/nome/:nome', function(req, res, next) {
-  const db = require('../db');
-  const Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
-  Customer.find({ nome: req.params.nome}).lean().exec(function(e,docs){
-    res.json(docs);
-    res.end();
-  });  
-});
+    }).catch(err => {
+      res.status(409).json({ error: err.message})
+      res.end()
+    })  
+})
 
-router.get('/api/id/:_id', function (req, res, next) {
-  const db = require('../db');
-  const Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
-  Customer.findById(req.params._id, function (e, docs) {
-      res.json(docs);
-      res.end();
-  });
-});
+router.get('/planet/nome/:nome', (req, res) => {
+  planetName = req.params.nome
+  planet_model.find({ nome: planetName}).lean().exec()
+    .then(data => {
+      if (data.length !== 0)
+        res.json(data)
+      else
+        res.status(409).json({ error: 'Document not found'})
+    })
+  })  
 
-router.post('/api/', function (req, res, next) {
-  const db = require('../db');
-  const Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
-  const newcustomers = new Customer ({ nome : req.body.nome, clima : req.body.clima, terreno : req.body.terreno});
-  newcustomers.save(function (err) {
-    if (err) {
-      res.status(500).json({ error: err.message});
-      res.end();
-      return;
-    }
-    res.json(newcustomers);
-    res.end();
-  });
-});
+router.get('/planet/id/:_id', async (req, res) => {
+  id = req.params._id
+  planet_model.findById(id).lean().exec()
+    .then(data => {
+      if (data !== null)
+        res.json(data)
+      else   
+        res.status(409).json({ error: 'Document not found'})
+  })
+})
 
-router.delete('/api/del/nome/:nome', function (req, res, next) {
-  const db = require('../db');
-  const Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
-  Customer.find({ nome: req.params.nome }).remove(function (err) {
-      if (err) {
-          res.status(500).json({ error: err.message });
-          res.end();
-          return;
-      }
-      res.json({success: true});
-      res.end();
-  });
-});
+router.post('/planet/create', (req, res) => {
+  const planet = new planet_model ({ 
+                                    nome : req.body.nome, 
+                                    clima : req.body.clima, 
+                                    terreno : req.body.terreno})
+  console.log(planet);
+  
+  planet.save()
+    .then(() => {
+      res.json(planet)
 
-module.exports = router;
+    }).catch( err => {
+      res.status(409).json({ error: err.message});
+      res.end()
+    })
+  })
+
+router.delete('/planet/del/nome/:nome', (req, res) => {
+  planet_name = req.params.nome
+  planet_model.deleteOne({ nome: planet_name }).exec() 
+    .then(data => {
+      if (data.deletedCount != 0)
+        res.json({ 
+                  success: true,
+                  deletedCount: data.deletedCount })
+      else 
+        res.json({ 
+                  success: true,
+                  deletedCount: data.deletedCount })
+    })
+})
+
+module.exports = router
