45a188663bbcafb54dcf27ca21be64a5409a4671
lucas3ms <lucas3ms.ls@gmail.com>
2020-02-04 10:14:23 -0300

    Colocando database no middleware

---
+++ src/middleware/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+module.exports = app => {
+    mongoose
+        .connect('mongodb://localhost/planet', { 
+            useCreateIndex: true, 
+            useNewUrlParser: true })
+        .then(console.log(`MongoDB connected`)
+        )
+        .catch(err => {
+            console.error(err)
+            process.exit(1)
+        })
+
+    return (ctx, next) => {
+        return next()
+    }
+}
