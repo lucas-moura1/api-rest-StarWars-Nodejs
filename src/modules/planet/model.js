e683546e4148c4575381b1f558d7bd1cf330a03e
lucas3ms <lucas3ms.ls@gmail.com>
2020-02-04 10:14:57 -0300

    Separação do modelo planeta

---
+++ src/modules/planet/model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const planetSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    clima: {
+        type: String,
+        required: true
+    },
+    terreno: {
+        type: String,
+        required: true
+    },
+    quantidadeDeFilmes: {
+        type: Number,
+        required: false
+    }
+},
+    { versionKey: false }
+)
+
+module.exports =  mongoose.model('planet', planetSchema)
