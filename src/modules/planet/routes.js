ecb4e3f0319e93ac0a64ad9f7e1c648e227f80d3
lucas3ms <lucas3ms.ls@gmail.com>
2020-02-06 10:15:40 -0300

    Adição da rota de update

--- src/modules/planet/routes.js
+++ src/modules/planet/routes.js
@@ -6,5 +6,7 @@ router.get('/planets', controller.list)
 router.get('/planet/name/:nome', controller.findPlanetByName)
 router.get('/planet/id/:id', controller.findPanetById)
 router.post('/planet/create', controller.create)
-router.patch('planet/id/:nome', controller.updateById)
+router.patch('/planet/id/:nome', controller.updateById)
 router.delete('/planet/id/:id', controller.delete)
+
+module.exports = router
