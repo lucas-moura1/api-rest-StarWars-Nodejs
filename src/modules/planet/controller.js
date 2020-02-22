8bd85515da7324f303b42d753eada583b9d75965
lucas3ms <lucas3ms.ls@gmail.com>
2020-02-22 13:21:03 -0300

    Corrigindo erro de estilo de codigo

--- src/modules/planet/controller.js
+++ src/modules/planet/controller.js
@@ -2,85 +2,82 @@ const Planet = require('./model')
 const requestStarWarsApi = require('../../lib/requestStartWarsApi')
 
 module.exports = {
-  async list(req, res) {
-    
+  async list (req, res) {
     await Planet.find().lean().exec()
-    .then(data => {
-      res.json(data)
-    })
-    .catch(err => {
-      res.status(409).json({ error: err.message})
-      res.end()
-    }) 
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        res.status(409).json({ error: err.message })
+        res.end()
+      })
   },
 
-  async findPanetById(req, res) {
-    planetId = req.params.id
+  async findPanetById (req, res) {
+    const planetId = req.params.id
 
     Planet.findById(planetId).lean().exec()
-    .then(data => {
-      if (data !== null)
-      res.json(data)
-      else
-      res.status(409).json({ error: 'Document not found'})
-    })
-
+      .then(data => {
+        if (data !== null) {
+          res.json(data)
+        } else res.status(409).json({ error: 'Document not found' })
+      })
   },
 
-  async findPlanetByName(req, res) {
-    planetName = req.params.nome
+  async findPlanetByName (req, res) {
+    const planetName = req.params.nome
 
-    Planet.find({ nome: planetName}).lean().exec()
-    .then(data => {
-      if (data.length !== 0)
-      res.json(data)
-      else
-      res.status(409).json({ error: 'Document not found'})
-    })
+    Planet.find({ nome: planetName }).lean().exec()
+      .then(data => {
+        if (data.length !== 0) {
+          res.json(data)
+        } else res.status(409).json({ error: 'Document not found' })
+      })
   },
 
-  async create(req, res) {
+  async create (req, res) {
     const planet = new Planet({
-      nome : req.body.nome,
-      clima : req.body.clima,
-      terreno : req.body.terreno})
+      nome: req.body.nome,
+      clima: req.body.clima,
+      terreno: req.body.terreno
+    })
 
-      await planet.save()
+    await planet.save()
       .then(() => {
         res.status(200).json(planet)
       })
-      .catch( err => {
-        res.status(409).json({ error: err.message});
+      .catch(err => {
+        res.status(409).json({ error: err.message })
         res.end()
       })
-    },
+  },
 
-  async updateById(req, res) {
+  async updateById (req, res) {
     const plantId = req.params.id
-    console.log(req.body);
-    
-    Planet.findByIdAndUpdate(JSON.parse(plantId), { $set: req.body}, (err, res) => {
+    console.log(req.body)
+
+    Planet.findByIdAndUpdate(JSON.parse(plantId), { $set: req.body }, (err, res) => {
       if (!err) res.json(res)
-      else res.json({ error: res.message})
+      else res.json({ error: res.message })
     })
   },
 
-  async delete(req, res) {
-    planetName = req.params.nome
+  async delete (req, res) {
+    const planetName = req.params.nome
 
-    Planet.deleteOne({ nome: planetName }).exec() 
-    .then(data => {
-      if (data.deletedCount != 0) {
-        res.json({ 
-          success: true,
-          deletedCount: data.deletedCount 
-        })
-      } else {
-        res.json({
-          success: true,
-          deletedCount: data.deletedCount 
-        })
-      }
-    })
+    Planet.deleteOne({ nome: planetName }).exec()
+      .then(data => {
+        if (data.deletedCount !== 0) {
+          res.json({
+            success: true,
+            deletedCount: data.deletedCount
+          })
+        } else {
+          res.json({
+            success: true,
+            deletedCount: data.deletedCount
+          })
+        }
+      })
   }
 }
