208932a7773d30d69050d8eed578d90000f5b037
lucas3ms <lucas3ms.ls@gmail.com>
2020-05-14 21:23:59 -0300

    Adição de autorização em cada rota de planeta

--- src/modules/planet/routes.js
+++ src/modules/planet/routes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('./controller')
+const auth = require('../../middleware/auth')
 
-router.get('/planets', controller.list)
-router.get('/planet/name/:nome', controller.findPlanetByName)
-router.get('/planet/id/:id', controller.findPanetById)
-router.post('/planet/create', controller.create)
-router.patch('/planet/id/:nome', controller.updateById)
-router.delete('/planet/id/:id', controller.delete)
+router.get('/planets', auth, controller.list)
+router.get('/planet/name/:nome', auth, controller.findPlanetByName)
+router.get('/planet/id/:id', auth, controller.findPanetById)
+router.post('/planet/create', auth, controller.create)
+router.patch('/planet/id/:nome', auth, controller.updateById)
+router.delete('/planet/id/:id', auth, controller.delete)
 
 module.exports = router
