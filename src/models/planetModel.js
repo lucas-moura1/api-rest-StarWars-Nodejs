68dca38960a27a59d599c6cc30c8cc41077d11d0
Lucas Moura <lucas3ms.ls@gmail.com>
2021-06-01 18:11:28 -0300

    Ajuste no modelo de planeta

---
+++ src/models/planetModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('../config/database')
+
+const planetSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    weather: {
+        type: String,
+        required: true
+    },
+    ground: {
+        type: String,
+        required: true
+    },
+    numberOfFilms: {
+        type: Number,
+        required: true
+    }
+},
+{ versionKey: false }
+)
+
+module.exports = mongoose.model('Planet', planetSchema)
